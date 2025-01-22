29905b5acf38ed7886cbc7bae292a2035f54dca6
opi1001 <mylaptop162084@gmali.com>
2025-01-22 22:01:41 +0600

    Develop The Design Country Currency pickers Package installing

--- lib/ui/Bottom_Navigations_Bar/home.dart
+++ lib/ui/Bottom_Navigations_Bar/home.dart
@@ -1,3 +1,6 @@
+import 'package:country_currency_pickers/country.dart';
+import 'package:country_currency_pickers/country_pickers.dart';
+import 'package:country_currency_pickers/currency_picker_dropdown.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,11 +11,50 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String selectedCurrency = 'USD';
+
+  Widget _buildCurrencyDropdownItem(Country country) => Container(
+        child: Row(
+          children: <Widget>[
+            CountryPickerUtils.getDefaultFlagImage(country),
+            const SizedBox(
+              width: 8.0,
+            ),
+            Text("${country.currencyName}"),
+          ],
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Page'),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          const SizedBox(height: 8),
+          const Center(
+            child: Text(
+              "Base Currency",
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
+          const SizedBox(height: 8),
+          Container(
+            margin: const EdgeInsets.all(8),
+            child: CountryPickerDropdown(
+              initialValue: 'us',
+              itemBuilder: _buildCurrencyDropdownItem,
+              onValuePicked: (Country? country) {
+                setState(() {
+                  selectedCurrency = country?.currencyCode ?? "";
+                });
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
